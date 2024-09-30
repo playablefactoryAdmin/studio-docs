d28f59f69ea2e8e1b297449a2005bed5f6c15a7d
mapacarta <omergunduz123@hotmail.com>
2024-09-30 13:53:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+console.log("script.js loaded");
 function init() {
+	console.log("init called");
 	const hoverBoxes = document.querySelectorAll(".hover-box");
 	const hoverTitles = document.querySelectorAll(".hover-box .title");
+	console.log(hoverTitles);
 	hoverTitles.forEach((title) => {
 		title.addEventListener("mouseover", () => {
 			hoverBoxes.forEach((hoverBox) => {
@@ -15,7 +18,7 @@ function init() {
 		if (hoverBox0) {
 			const hoverParentRect = hoverBox0.parentElement.getBoundingClientRect();
 			let scale = Math.min(hoverParentRect.width / 30, hoverParentRect.height / 30) * 0.05;
-            scale = Math.max(0.5, scale);
+			scale = Math.max(0.5, scale);
 			scale = Math.min(1, scale);
 
 			hoverTitles.forEach((title) => {
