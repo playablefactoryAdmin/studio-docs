91dc8f93e602d2b595f4c0a097a6e44eb0010cff
Mehmet Can <mehmetcan-akbay@outlook.com>
2025-07-17 12:58:15 +0300

    fix to photos?

--- src/components/NavbarLogo.tsx
+++ src/components/NavbarLogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NavbarLogo() {
     return (
         <div className="flex flex-row items-center gap-2  opacity-0 transition-all animate-slide-in ">
-            <Image src="/logo.svg" alt="Studio Docs" width={40} height={50}/>
+            <Image src="studio-docs/logo.svg" alt="Studio Docs" width={40} height={50}/>
             <p className="text-xl font-light">Studio Docs</p>
         </div> 
     )
